6e2ad05783781fc97ed2a86c7dad66dc59aaa4c6
Shubham Bhardwaj <bhardwajshub0001@gmail.com>
2026-02-11 11:48:13 +0530

    6th DONE >> dry run on your mind first then click on run button

--- src/06-shipping-calculator.js
+++ src/06-shipping-calculator.js
@@ -30,4 +30,29 @@
  */
 export function calculateShipping(weight, country, orderTotal) {
   // Your code here
+  if (weight <= 0 || orderTotal <= 0) {
+    return -1
+  }
+
+  if (country === "US" && orderTotal > 50) {
+    return 0
+  }
+  
+  if (country !== "US" && orderTotal > 100) {
+    return 0
+  }
+
+  if (weight > 0 && weight <= 1) {
+    return country === "US" ? 5 : 15
+  }
+
+  if (weight > 1 && weight <= 5) {
+    return country === "US" ? 10 : 25
+  }
+
+  if (weight > 5 ) {
+    return country === "US" ? 15 : 40
+  }
+
+  
 }
