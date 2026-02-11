eeab88004d28a5411083148ba39c3d5e424b4473
Shubham Bhardwaj <bhardwajshub0001@gmail.com>
2026-02-11 23:05:14 +0530

    10th complete

--- src/10-tip-calculator.js
+++ src/10-tip-calculator.js
@@ -30,5 +30,36 @@
  * @returns {{ tipPercentage: number, tipAmount: number, totalAmount: number } | null}
  */
 export function calculateTip(billAmount, serviceRating) {
+  if (billAmount <= 0 ) {
+    return null
+  }
+
+  if (!Number.isInteger(serviceRating) || serviceRating < 1 || serviceRating > 5 ) {
+    return null
+  }
+
   // Your code here
+    if (serviceRating == 1) {
+      let tip = billAmount * 0.05; 
+      tip = Math.round(tip * 100) /100
+      let total = billAmount + tip;
+      total = Math.round(total * 100) /100
+     return { tipPercentage: 5, tipAmount: tip, totalAmount: total }; 
+  } else if (serviceRating == 2) {
+     let tip = billAmount * 0.10; 
+      let total = billAmount + tip;
+     return { tipPercentage: 10, tipAmount: tip, totalAmount: total }; 
+  } else if (serviceRating == 3) {
+     let tip = billAmount * 0.15; 
+      let total = billAmount + tip;
+     return { tipPercentage: 15, tipAmount: tip, totalAmount: total }; 
+  } else if (serviceRating == 4) {
+     let tip = billAmount * 0.20; 
+      let total = billAmount + tip;
+     return { tipPercentage: 20, tipAmount: tip, totalAmount: total }; 
+  } else if (serviceRating == 5) {
+     let tip = billAmount * 0.25; 
+      let total = billAmount + tip;
+     return { tipPercentage: 25, tipAmount: tip, totalAmount: total }; 
+  } 
 }
